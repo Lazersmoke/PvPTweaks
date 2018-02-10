a92e37f0c2aab7f7d855eeeeefe3f28cafed2097
Lazersmoke <lazersmoke@gmail.com>
2018-02-10 17:31:17 -0600

    Fix regen tweaks

--- src/main/java/com/civclassic/pvptweaks/tweaks/GeneralTweaks.java
+++ src/main/java/com/civclassic/pvptweaks/tweaks/GeneralTweaks.java
@@ -1,48 +1,42 @@
 package com.civclassic.pvptweaks.tweaks;
 
-import java.util.UUID;
-
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
+import org.bukkit.Bukkit;
 
 import com.civclassic.pvptweaks.PvPTweaks;
 import com.civclassic.pvptweaks.Tweak;
 
-import vg.civcraft.mc.civmodcore.util.cooldowns.ICoolDownHandler;
-import vg.civcraft.mc.civmodcore.util.cooldowns.MilliSecCoolDownHandler;
-
 public class GeneralTweaks extends Tweak {
 
 	private double healthRegen;
-	private ICoolDownHandler<UUID> regenCds;
+	private float exhaustionCost;
+	private Integer bukkitTaskId;
 	
 	public GeneralTweaks(PvPTweaks plugin, ConfigurationSection config) {
 		super(plugin, config);
 	}
 
-	@EventHandler
-	public void onEntityRegainHealth(EntityRegainHealthEvent event) {
-		if(event.getRegainReason() != RegainReason.REGEN || event.getRegainReason() != RegainReason.SATIATED
-				|| event.getEntityType() != EntityType.PLAYER) {
-			return;
-		}
-		Player player = (Player) event.getEntity();
-		if(regenCds.onCoolDown(player.getUniqueId())) {
-			event.setCancelled(true);
-		} else {
-			event.setAmount(healthRegen);
-			regenCds.putOnCoolDown(player.getUniqueId());
-		}
-	}
-	
 	@Override
 	public void loadConfig(ConfigurationSection config) {
 		healthRegen = config.getDouble("healthRegen");
-		regenCds = new MilliSecCoolDownHandler<UUID>(config.getLong("regenDelay"));
+		exhaustionCost = (float) config.getDouble("exhaustionCost");
+		long regenFrequency = config.getLong("regenDelay");
+		if(bukkitTaskId != null){
+			// Cancel old task
+			Bukkit.getScheduler().cancelTask(bukkitTaskId);
+		}
+		bukkitTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin(), new Runnable() {
+			@Override
+			public void run() {
+				Bukkit.getOnlinePlayers().forEach(p -> {
+					p.getWorld().setGameRuleValue("naturalRegeneration","false");
+					if(p.getFoodLevel() >= 18 && p.getHealth() < p.getMaxHealth()){
+						p.setHealth(Math.min(p.getMaxHealth(),p.getHealth() + healthRegen));
+						p.setExhaustion(p.getExhaustion() + exhaustionCost);
+					}
+				});
+			}
+}, 1L, regenFrequency);
 	}
 
 	@Override
