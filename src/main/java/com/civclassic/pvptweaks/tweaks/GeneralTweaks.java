cd3852be8eac4537ebcd32490470ff4ee42671c9
Lazersmoke <lazersmoke@gmail.com>
2018-02-14 15:33:11 -0600

    Tweak the regen tweak a bit

--- src/main/java/com/civclassic/pvptweaks/tweaks/GeneralTweaks.java
+++ src/main/java/com/civclassic/pvptweaks/tweaks/GeneralTweaks.java
@@ -10,6 +10,7 @@ public class GeneralTweaks extends Tweak {
 
 	private double healthRegen;
 	private float exhaustionCost;
+	private long regenFrequency;
 	private Integer bukkitTaskId;
 	
 	public GeneralTweaks(PvPTweaks plugin, ConfigurationSection config) {
@@ -20,29 +21,29 @@ public class GeneralTweaks extends Tweak {
 	public void loadConfig(ConfigurationSection config) {
 		healthRegen = config.getDouble("healthRegen");
 		exhaustionCost = (float) config.getDouble("exhaustionCost");
-		long regenFrequency = config.getLong("regenDelay");
+		regenFrequency = config.getLong("regenDelay");
 		if(bukkitTaskId != null){
 			// Cancel old task
 			Bukkit.getScheduler().cancelTask(bukkitTaskId);
 		}
-		bukkitTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin(), new Runnable() {
-			@Override
-			public void run() {
-				Bukkit.getOnlinePlayers().forEach(p -> {
-					p.getWorld().setGameRuleValue("naturalRegeneration","false");
-					if(p.getFoodLevel() >= 18 && p.getHealth() < p.getMaxHealth()){
-						p.setHealth(Math.min(p.getMaxHealth(),p.getHealth() + healthRegen));
-						p.setExhaustion(p.getExhaustion() + exhaustionCost);
-					}
-				});
-			}
-}, 1L, regenFrequency);
+		// Disable natural regen completely
+		Bukkit.getServer().getWorlds().forEach(w -> w.setGameRuleValue("naturalRegeneration","false"));
+		bukkitTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin(), () -> {
+			Bukkit.getOnlinePlayers().forEach(p -> {
+				if(p.getFoodLevel() >= 18 && p.getHealth() < p.getMaxHealth()){
+					p.setHealth(Math.min(p.getMaxHealth(),p.getHealth() + healthRegen));
+					p.setExhaustion(p.getExhaustion() + exhaustionCost);
+				}
+			});
+		}, 1L, regenFrequency);
 	}
 
 	@Override
 	protected String status() {
 		StringBuilder status = new StringBuilder();
 		status.append("  healthRegen: ").append(healthRegen);
+		status.append("  regenDelay: ").append(regenFrequency);
+		status.append("  exhaustionCost: ").append(exhaustionCost);
 		return status.toString();
 	}
 }
